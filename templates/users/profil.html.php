080da98d704c2451214c072ece681f91fb84f2e0
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-06-29 16:06:40 +0200

    page profil

--- templates/users/profil.html.php
+++ templates/users/profil.html.php
@@ -1,5 +1,6 @@
 <?php $user = $_SESSION['user'];?> 
     <h1><?php echo $user['userName']; ?></h1> 
     <h5><?php echo $user['userEmail']; ?></h5>
-                            
+    <hr>
+<a href="index.php?controller=gateau&task=index" class="btn btn-primary">Retour</a>
                             
